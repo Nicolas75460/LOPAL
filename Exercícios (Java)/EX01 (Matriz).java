a9c8b193beb092dc77e262fdcd12f0012f10cd8b
Nícolas Oliveira Carvalho da Silva <159180162+Nicolas75460@users.noreply.github.com>
2024-06-04 10:53:08 -0300

    Add files via upload

---
+++ Exercícios (Java)/EX01 (Matriz).java
@@ -0,0 +1,24 @@
+public class EX01
+{
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // Declaração de matriz de clientes com 3 clientes e 2 atributos
+        
+        String[][] clientes = new String[3][2];
+        
+        // Atribuição de valores aos clientes
+        clientes[0][0] = "João";
+        clientes[0][1] = "Rua das Flores, 123";
+        clientes[1][0] = "Maria";
+        clientes[1][1] = "Avenida dos Anjos, 456";
+        clientes[2][0] = "Pedro";
+        clientes[2][1] = "Praça da Liberdade, 789";
+        
+        // Impressão dos clientes
+        for(int i = 0; i < 3; i++){
+            System.out.println("Nome: " + clientes[i][0]);
+            System.out.println("Endereço: " + clientes[i][1]);
+            System.out.println();
+        }
+    }
+}
